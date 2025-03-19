5f6c1d253893dbf90b36b2f5a8be89e0ea6ea398
Ana Clara Fontes <clara.fontes.ac@gmail.com>
2025-03-19 14:42:48 -0400

    trocando objetp por titulo no Admin Panel

--- posts/models.py
+++ posts/models.py
@@ -5,3 +5,7 @@ class Post(models.Model):
     post_title = models.CharField(max_length=60)
     post_content = models.TextField()
     publish_date = models.DateField(auto_now=True) # Automaticamente atualiza a data em cada atualização
+
+    def __str__ (self):
+        return self.post_title
+
