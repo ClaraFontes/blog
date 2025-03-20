d1546c47cc60d50290aee0e4b27c8e245fab8781
Ana Clara Fontes <clara.fontes.ac@gmail.com>
2025-03-20 14:51:13 -0400

    salvando dados no banco de dados através do formulário preenchido.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.EmailField()
+    telefone = models.IntegerField()
+    bio = models.TextField()
--- users/views.py
+++ users/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from .forms import UsersForm
 from django.http import HttpResponseRedirect, HttpResponse
+from .models import User
 
 # Create your views here.
 def home(request):
     if request.method == 'POST':
         form = UsersForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data['nome'])
-            # lógica
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            telefone = form.cleaned_data['telefone']
+            bio = form.cleaned_data['bio']
+
+            user = User.objects.create(nome=nome, email=email, telefone=telefone, bio=bio)
+
             return HttpResponseRedirect('/users/obrigado/')
     else:
         form = UsersForm()
