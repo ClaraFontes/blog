c88e4792696461226676ef4ca668cbd108842cda
Ana Clara Fontes <clara.fontes.ac@gmail.com>
2025-03-20 12:37:49 -0400

    is_valid e cleaned_data properties

--- users/views.py
+++ users/views.py
@@ -4,7 +4,9 @@ from .forms import UsersForm
 # Create your views here.
 def home(request):
     if request.method == 'POST':
-        print(request.POST)
-        form = UsersForm()
+        form = UsersForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+
     form = UsersForm()
     return render(request, 'users/home.html', {'form': form})
