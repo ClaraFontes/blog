75d9ea0ff2f71d1cee8468d56a8f29aaa770caff
Ana Clara Fontes <clara.fontes.ac@gmail.com>
2025-03-20 16:37:36 -0400

    método clean no ModelForm

--- users/forms.py
+++ users/forms.py
@@ -30,7 +30,16 @@ class UsersForm(forms.ModelForm):
                 'required':'Este campo é obrigatório!'
             }
         }
+    def clean(self):
+        cleaned_data = super().clean()
+        nome = self.cleaned_data['nome']
+        email = self.cleaned_data['email']
 
+        if nome[0] != 'c' and nome[0] != 'C':
+            raise forms.ValidationError("A primeira letra do nome deve ser 'C'.")
+        if email[0] != 'c' and email[0] != 'C':
+            raise forms.ValidationError("A primeira letra do email deve ser 'C'.")
+        
     # nome = forms.CharField(validators=[validators.MaxLengthValidator(10), start_with_c],min_length=5, label="Seu Nome", label_suffix="", error_messages={'required': "Seu nome não pode estar vazio!", 'min_length':"O tamanho mínmo para este campo é de 5 caracteres."}, widget=forms.TextInput(attrs={'class':'form-control'}))
     
     # email = forms.EmailField(required=False, label="Seu Email", label_suffix="", help_text="Apenas emails do Gmail são aceitos.", widget=forms.EmailInput(attrs={'placeholder':'Insira seu melhor email', 'class':'form-control'}), validators=[start_with_c])
@@ -39,12 +48,4 @@ class UsersForm(forms.ModelForm):
 
     # bio = forms.CharField(widget=forms.Textarea(attrs={'cols':20, 'placeholder':'Bio', 'class':'form-control'}))
 
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     nome = self.cleaned_data['nome']
-    #     email = self.cleaned_data['email']
-
-    #     if nome[0] != 'c' and nome[0] != 'C':
-    #         raise forms.ValidationError("A primeira letra do nome deve ser 'C'.")
-    #     if email[0] != 'c' and email[0] != 'C':
-    #         raise forms.ValidationError("A primeira letra do email deve ser 'C'.")
+    
