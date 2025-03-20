3069e79624248a004e6dbe9b82694ef84570f1f6
Ana Clara Fontes <clara.fontes.ac@gmail.com>
2025-03-20 13:06:04 -0400

    customizando propriedades dos campos

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,6 @@
 from django import forms
 
 class UsersForm(forms.Form):
-    nome = forms.CharField()
+    nome = forms.CharField(min_length=5, label="Seu Nome", label_suffix="", error_messages={'required': "Seu nome não pode estar vazio!", 'min_length':"O tamanho mínmo para este campo é de 5 caracteres."})
+    email = forms.EmailField(required=False, label="Seu Email", label_suffix="", help_text="Apenas emails do Gmail são aceitos.")
+    telefone = forms.IntegerField(label="Número para Contato", label_suffix="")
