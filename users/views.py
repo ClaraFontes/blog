b63a21eec1576a42afcf3387c25e689c2fce4d14
Ana Clara Fontes <clara.fontes.ac@gmail.com>
2025-03-20 12:46:29 -0400

    enviando o form

--- users/views.py
+++ users/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from .forms import UsersForm
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
 def home(request):
     if request.method == 'POST':
         form = UsersForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
-
-    form = UsersForm()
+            print(form.cleaned_data['nome'])
+            # lógica
+            return HttpResponseRedirect('/users/obrigado/')
+    else:
+        form = UsersForm()
     return render(request, 'users/home.html', {'form': form})
+
+def obrigado(request):
+    return HttpResponse("FORMS ENVIADO!!")
