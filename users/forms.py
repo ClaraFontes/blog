b556e3a16bb3b3b2fc33b9bd12eb362e15136822
Ana Clara Fontes <clara.fontes.ac@gmail.com>
2025-03-20 14:20:25 -0400

    utilizando widgets e estilizando o formulário com bootstrap

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 
 class UsersForm(forms.Form):
-    nome = forms.CharField(min_length=5, label="Seu Nome", label_suffix="", error_messages={'required': "Seu nome não pode estar vazio!", 'min_length':"O tamanho mínmo para este campo é de 5 caracteres."})
+    nome = forms.CharField(min_length=5, label="Seu Nome", label_suffix="", error_messages={'required': "Seu nome não pode estar vazio!", 'min_length':"O tamanho mínmo para este campo é de 5 caracteres."}, widget=forms.TextInput(attrs={'class':'form-control'}))
     
-    email = forms.EmailField(required=False, label="Seu Email", label_suffix="", help_text="Apenas emails do Gmail são aceitos.")
+    email = forms.EmailField(required=False, label="Seu Email", label_suffix="", help_text="Apenas emails do Gmail são aceitos.", widget=forms.EmailInput(attrs={'placeholder':'Insira seu melhor email', 'class':'form-control'}))
     
-    telefone = forms.IntegerField(label="Número para Contato", label_suffix="")
+    telefone = forms.IntegerField(label="Número para Contato", label_suffix="", widget=forms.NumberInput(attrs={'class':'form-control'}))
+
+    bio = forms.CharField(widget=forms.Textarea(attrs={'cols':20, 'placeholder':'Bio', 'class':'form-control'}))
